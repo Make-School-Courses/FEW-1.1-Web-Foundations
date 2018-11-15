d709b490533bb1ed9bc26c5590092b388f0fa1f0
Mitchell <soggybag@gmail.com>
2018-11-15 09:12:55 -0800

    added stretch goals

--- 08-JavaScript-Arrays-Objects-Loops-and-Logic/example/main.js
+++ 08-JavaScript-Arrays-Objects-Loops-and-Logic/example/main.js
@@ -27,3 +27,40 @@
 // Images should include a width, and height. 
 // It should be represented as an Object with a url, width, and height
 // Add the object to the DOM
+
+
+// Include a date for the project. 
+// This should be a date object! 
+// Display the date in the DOM
+
+
+// Format the date into a something nicer
+
+
+// Add some styles
+
+
+
+// Stretch Goals 
+// Make an array of project objects
+// Loop through this array and generate an HTML element for each!
+
+
+
+// Inlcude a new property in each project: tags. This should be an
+// array of strings. Imagine these as strings keywords describing
+// the project. It might look this: ['css', 'html', 'javascript']
+// Display these keywords in the project element. 
+
+
+
+
+
+
+
+
+
+
+
+
+
