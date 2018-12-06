8d7160d97f11120b460af81e955a84b5e58451ba
Mitchell <soggybag@gmail.com>
2018-12-05 20:11:57 -0800

    update

--- solutions/tip-calculator/main.js
+++ solutions/tip-calculator/main.js
@@ -20,6 +20,9 @@ function click_calculate(e) {
 	// Get values from inputs 
 	const percent = Number(percent_input.value) // Convert these to numbers
 	const amount = Number(amount_input.value)
+	
+	// TODO: Needs some validation here
+	
 	const tip = amount * percent / 100
 	const total = amount + tip
 	tip_display.innerHTML = tip.toFixed(2)
